3b6ac03b746a2946b614379af281cc1d86f71363
brandonparker1369 <loopsforsky@gmail.com>
2026-06-25 20:21:32 -0400

    Let's go!! 106/106 passing! That's everything working perfectly! Commit right now with: fixed shopping cart GET, POST, and DELETE methods in ShoppingCartController and ShoppingCartService

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,6 +1,7 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.yearup.models.CartItem;
 import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
@@ -63,6 +64,7 @@ public class ShoppingCartService
         return getByUserId(userId);
     }
 
+    @Transactional
     public void clearCart(int userId)
     {
         shoppingCartRepository.deleteByUserId(userId);
